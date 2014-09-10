59fec5f87f67f840b721685f9b98759e6f3eaa68
Noa Rave <noa.rave@berlinonline.de>
2014-09-10 16:37:23 +0200

    corrections to organism.rb

--- organism.rb
+++ organism.rb
@@ -3,20 +3,20 @@
    def initialize(name)
      ## cat has-a name
      @name = name
-     
+     puts "Am organism has been created."
    end  
  end
  
  class Flora < Organism
  
-   def plant
+   def description
      puts "#{@name} is a plant."
    end
  end
  
  class Fauna < Organism
     
-    def animal
+    def description
       puts "#{@name} is an animal."
     end
  end
@@ -25,22 +25,27 @@
  class LandPlants < Flora
  
  
-   def land_plant
+   def description
      puts "#{@name} is a plant which grows on or under land."
    end
  end
  
  class WaterPlants < Flora
    
-   def water_plant
+   def description
      puts "a #{@name} is a plant which grows either on, or under water." 
    end
  end
  
  
  class Trees < LandPlants
- 
-   def tree
+   def initialize(name)
+     super(name)
+     print "it's a tree"
+   end
+
+   def description
+   super() 
      puts "a #{@name} is a plant of type tree"
    end
    
@@ -48,21 +53,21 @@
  
  class Vegetables < LandPlants
    
-   def vegetable
+   def description
    puts "a #{@name} is a plant of type vegetable"
    end
  end 
  
  class Mammals < Fauna
    
-   def mammal
+   def description
     puts "#{@name} is a mammal and therefore doesn't lay any eggs."
    end
  end
  
  class Birds < Fauna
  
-   def bird
+   def description
      puts "#{@name} is a type of bird, therefore it has wings."
    end
  end
@@ -76,21 +81,22 @@
  
  class Bears < Mammals
  
-   def bear
+   def description
      puts "#{@name} is a mammal of type bear."
    end
  end
  
  class Ducks < Birds
  
-   def duck
+   def description
      puts "#{@name} is a bird of type duck."
    end  
  end 
  
  class Frogs < Amphibians
  
-   def frog
+   def description
+   
      puts "#{@name} is an amphibian of type frog. it is presumably green."
    end
  end
@@ -105,10 +111,7 @@
  oak = Trees.new("Oak")
  
  
- cactus.plant
- waterlilly.plant
- waterlilly.water_plant
- kermit.animal
- kermit.amphibian
- kermit.frog
- oak.land_plant
+ cactus.description
+ waterlilly.description
+ kermit.description
+ oak.description
