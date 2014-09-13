30b118535ee22e084c0ea322ef1cc4e92f663f5d
Dany Shaanan <danyshaanan@gmail.com>
2014-09-14 00:21:18 +0300

    added a loop

--- ex34.rb
+++ ex34.rb
@@ -1,12 +1,9 @@
  animals = ['bear','ruby','peacock','kangaroo','whale','platypus']
+ ordinals = ['1st', '2nd', '3rd', '4th', '5th', '6th']
  
- 
- puts "The 1st animal is #{animals[0]} and has the cardinal number of 0"
- puts "The 2nd animal is #{animals[1]} and has the cardinal number of 1"
- puts "The 3rd animal is #{animals[2]} and has the cardinal number of 2"
- puts "The 4th animal is #{animals[3]} and has the cardinal number of 3"
- puts "The 5th animal is #{animals[4]} and has the cardinal number of 4"
- puts "The 6th animal is #{animals[5]} and has the cardinal number of 5"
+(0..5).each do |i|
+   puts "The #{ordinals[i]} animal is #{animals[i]} and has the cardinal number of #{i}"
+end
 
 # ruby
 # peacock
